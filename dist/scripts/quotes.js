8b1f8262011a50855c3e4cda9fcc31a4e6f9c315
Anthony <anthony.siletti@gmail.com>
2020-02-14 21:51:42 -0500

    added dictionary, changed colors

--- dist/scripts/quotes.js
+++ dist/scripts/quotes.js
@@ -1,6 +1,6 @@
 class dailyQuotes {
   async getDailyQuote() {
-    const response = await fetch(`http://quotes.rest/qod.json`);
+    const response = await fetch(`https://quotes.rest/qod.json`);
     const resData = await response.json();
     return resData;
   }
@@ -8,7 +8,7 @@ class dailyQuotes {
 
 class chuckNorrisQuotes {
   async getChuckNorrisQuotes(number) {
-    const response = await fetch(`http://api.icndb.com/jokes/random/${number}`);
+    const response = await fetch(`https://api.icndb.com/jokes/random/${number}`);
     const resData = await response.json();
     return resData;
   }
